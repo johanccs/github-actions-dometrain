8ab24d056a8d1f9e5c89141981a9175056900b66
Johan Potgieter <johan.ccs@gmail.com>
2024-12-15 11:51:47 +0200

    Add controller and model

---
+++ github-actions/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace github_actions.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        
+    }
+}
