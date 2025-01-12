60b207009552e95128ea49bece8360a44962238f
Johan Potgieter <johan.ccs@gmail.com>
2025-01-12 11:50:44 +0200

    Add deploy prod job

--- github-actions/Program.cs
+++ github-actions/Program.cs
@@ -9,8 +9,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
